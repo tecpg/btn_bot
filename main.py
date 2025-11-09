bcff2dd73b5ac37cdad1cfa2035c8007c0ae615a
tecpg <paschalgreen007@gmail.com/usr/local/bin/python3>
2025-11-09 19:17:46 +0100

    Add main.py worker script

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-import all_betcodes
-import logging
-import get_rightside_odds
-import tips_combo
-import oddslot
-import time as _time
+import time, logging
+import all_betcodes, get_rightside_odds, oddslot
 
-
-_runtime = 5
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
-def run():
+def run_tasks():
     logging.info("Starting daily task...")
     try:
         all_betcodes.run()
-        _time.sleep(_runtime)
+        time.sleep(5)
         oddslot.run()
-        _time.sleep(_runtime)
+        time.sleep(5)
         get_rightside_odds.run()
-        _time.sleep(_runtime)
-        tips_combo.run()
+        time.sleep(5)
+      
         logging.info("Task completed successfully.")
     except Exception as e:
         logging.error(f"Error during task: {e}")
 
 if __name__ == "__main__":
-    run()
+    run_tasks()
